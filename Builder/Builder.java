e001178cc256b2c98270e8915ed81acd603def6e
binitshrest <binitshrest@gmail.com>
2024-10-26 11:22:02 +0545

    learnt Builder Design pattern

--- Builder/Builder.java
+++ Builder/Builder.java
@@ -1,63 +1,68 @@
 package Builder;
 
+// ! This builder class is transferred in to Student.class.
+// ! This is the good coding practise
 
-public class Builder {
-    private String name;
-    private int age;
-    private double psp;
-    private String batch;
-    private String gradYear;
-    private String phone;
+// public class Builder {
+//     private String name;
+//     private int age;
+//     private double psp;
+//     private String batch;
+//     private String gradYear;
+//     private String phone;
 
-    public String getName() {
-        return name;
-    }
-    public Builder setName(String name) {
-        this.name = name;
-        return this;
-    }
-    public int getAge() {
-        return age;
-    }
-    public Builder setAge(int age) {
-        this.age = age;
-        return this;
-    }
-    public double getPsp() {
-        return psp;
-    }
-    public Builder setPsp(double psp) {
-        this.psp = psp;
-        return this;
-    }
-    public String getBatch() {
-        return batch;
-    }
-    public Builder setBatch(String batch) {
-        this.batch = batch;
-        return this;
-    }
-    public String getGradYear() {
-        return gradYear;
-    }
-    public Builder setGradYear(String gradYear) {
-        this.gradYear = gradYear;
-        return this;
-    }
-    public String getPhone() {
-        return phone;
-    }
-    public Builder setPhone(String phone) {
-        this.phone = phone;
-        return this;
-    }
+//     public String getName() {
+//         return name;
+//     }
+//     public Builder setName(String name) {
+//         this.name = name;
+//         return this;
+//     }
+//     public int getAge() {
+//         return age;
+//     }
+//     public Builder setAge(int age) {
+//         this.age = age;
+//         return this;
+//     }
+//     public double getPsp() {
+//         return psp;
+//     }
+//     public Builder setPsp(double psp) {
+//         this.psp = psp;
+//         return this;
+//     }
+//     public String getBatch() {
+//         return batch;
+//     }
+//     public Builder setBatch(String batch) {
+//         this.batch = batch;
+//         return this;
+//     }
+//     public String getGradYear() {
+//         return gradYear;
+//     }
+//     public Builder setGradYear(String gradYear) {
+//         this.gradYear = gradYear;
+//         return this;
+//     }
+//     public String getPhone() {
+//         return phone;
+//     }
+//     public Builder setPhone(String phone) {
+//         this.phone = phone;
+//         return this;
+//     }
 
-    public Student build(){
-        return new Student(this);
-    }
+//     public Student build(){
+//         return new Student(this);
+//     }
 
-    public static Builder builder(){
-        return new Builder();
-    }
-}
+//     public static Builder builder(){
+//         return new Builder();
+//     }
+// }
+
+
+
 
--- Builder/Client.java
+++ Builder/Client.java
@@ -2,12 +2,18 @@ package Builder;
 
 public class Client {
     public static void main(String[] args) {
-        Builder builder = Student.builder();
-        builder.setName("Binit");
-        builder.setBatch("Beginner");
-        builder.setAge(20);
+        // Builder builder = Student.builder();
+        // builder.setName("Binit");
+        // builder.setBatch("Beginner");
+        // builder.setAge(20);
 
-        Student student = builder.build();
+        Student student = Student
+                            .builder()
+                            .setName("Binit")
+                            .setAge(27)
+                            .setPsp(99.00)
+                            .build();
         
+        // Student.Builder  stu =  Student.builder();
     }
 }
